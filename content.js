1077c0f29930afc6fb73da00b081bf5977e8c7ed
MohammedFazilKhasim <fazilmohammed697@outlook.com>
2025-07-06 18:46:15 -0500

    Added anti-anti-adblock logic + refined selectors

--- content.js
+++ content.js
@@ -1,14 +1,38 @@
+(function antiAntiAdblock() {
+  // Fake some global variables that detect ad blockers
+  window.__defineGetter__('abpDetected', () => false);
+  window.__defineGetter__('hasAdBlock', () => false);
+
+  // Prevent YouTube from seeing that we're hiding elements
+  const origDescriptor = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetHeight');
+  Object.defineProperty(HTMLDivElement.prototype, 'offsetHeight', {
+    configurable: false,
+    get: function() {
+      if (this.className && this.className.includes('ad')) {
+        return origDescriptor.get.apply(this);
+      }
+      return 1;
+    }
+  });
+
+})();
+
 const adSelectors = [
-    ".ad-banner",
-    ".advertisement",
-    "[data-ad]",
-    "[id*='ad']",
-    "[class*='ad']",
-    "iframe[src*='ads']"
+  ".ad",
+  ".ads",
+  ".advertisement",
+  ".banner-ad",
+  ".ad-banner",
+  ".ads-container",
+  "[data-ad]",
+  "[data-ads]",
+  "iframe[src*='ads']",
+  "iframe[src*='doubleclick']",
+  "iframe[src*='googlesyndication']"
 ];
+
 adSelectors.forEach(selector => {
-    document.querySelectorAll(selector).forEach(el => {
-        el.style.display = "none";
-        el.remove();
-    });
+  document.querySelectorAll(selector).forEach(el => {
+    el.style.display = "none";
+  });
 });
