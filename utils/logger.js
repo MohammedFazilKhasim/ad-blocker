f58592988dc756a7d1042e9d9aea59649fc3bc11
MohammedFazilKhasim <fazilmohammed697@outlook.com>
2025-07-09 22:04:54 -0500

    consolidate logging across content/background scripts

---
+++ utils/logger.js
@@ -0,0 +1,11 @@
+export function log(...args) {
+  console.log("[Ad-bloc]", ...args);
+}
+
+export function warn(...args) {
+  console.warn("[Ad-bloc]", ...args);
+}
+
+export function error(...args) {
+  console.error("[Ad-bloc]", ...args);
+}
